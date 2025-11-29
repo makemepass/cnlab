ea98290075b972fdb29e3f7024623ba3e222c169
makemepass <jainnoshank@gmail.com>
2025-11-29 11:23:50 +0530

    Update bitstuffing.c

--- bitstuffing.c
+++ bitstuffing.c
@@ -4,55 +4,31 @@
 #define FLAG "01111110"
 
 int main() {
-    char input[100], stuffed[200], destuffed[200];
+    char in[100], stuff[200] = FLAG, destuff[200];
     int i, j = 0, count = 0;
 
     printf("Enter binary value: ");
-    scanf("%s", input);
-
-    // Start with FLAG
-    strcpy(stuffed, FLAG);
+    scanf("%s", in);
 
     // Bit Stuffing
-    for(i = 0; i < strlen(input); i++) {
-        int len = strlen(stuffed);
-        stuffed[len] = input[i];
-        stuffed[len + 1] = '\0';
-
-        if(input[i] == '1') {
-            count++;
-            if(count == 5) {
-                strcat(stuffed, "0");  // Stuff a 0
-                count = 0;
-            }
-        } else {
-            count = 0;
-        }
+    for(i = 0; in[i]; i++) {
+        strncat(stuff, &in[i], 1);
+        count = (in[i] == '1') ? count + 1 : 0;
+        if(count == 5) strcat(stuff, "0"), count = 0;
     }
 
-    // End with FLAG
-    strcat(stuffed, FLAG);
+    strcat(stuff, FLAG);
+    printf("\nStuffed: %s\n", stuff);
 
-    printf("\nStuffed: %s\n", stuffed);
-
-    // Bit Destuffing
-    j = 0;
-    count = 0;
-    for(i = strlen(FLAG); i < strlen(stuffed) - strlen(FLAG); i++) { // skip flags
-        destuffed[j++] = stuffed[i];
-        if(stuffed[i] == '1') {
-            count++;
-            if(count == 5) {
-                i++; // Skip stuffed 0
-                count = 0;
-            }
-        } else {
-            count = 0;
-        }
+    // Bit De-stuffing
+    int start = strlen(FLAG), end = strlen(stuff) - strlen(FLAG);
+    for(i = start; i < end; i++) {
+        destuff[j++] = stuff[i];
+        count = (stuff[i] == '1') ? count + 1 : 0;
+        if(count == 5) i++, count = 0;
     }
-    destuffed[j] = '\0';
+    destuff[j] = '\0';
 
-    printf("Destuffed: %s\n", destuffed);
-
+    printf("Destuffed: %s\n", destuff);
     return 0;
 }
