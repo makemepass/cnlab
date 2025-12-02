ffd52ec5d71d1ea40c8e49203aec2fbe8ea6da67
makemepass <jainnoshank@gmail.com>
2025-12-02 20:54:17 +0530

    Update link_state_routing.c

--- link_state_routing.c
+++ link_state_routing.c
@@ -1,71 +1,74 @@
-#include <stdio.h>
-#include <stdbool.h>
-
+#include<stdio.h>
+#include<limits.h>
 #define MAX 100
-#define INF 9999
+#define INF INT_MAX
 
-int minNode(int dist[], bool visited[], int n) {
-    int min = INF, index = -1;
-    for (int i = 0; i < n; i++)
-        if (!visited[i] && dist[i] < min)
-            min = dist[i], index = i;
-    return index;
-}
-
-void printPath(int parent[], int v) {
-    if (parent[v] == -1) return;
-    printPath(parent, parent[v]);
-    printf(" %c", v + 'a');
-}
-
-void dijkstraLSR(int g[MAX][MAX], int n, int src, int dest) {
-    int dist[MAX], parent[MAX];
-    bool visited[MAX];
-
-    for (int i = 0; i < n; i++)
-        dist[i] = INF, visited[i] = false, parent[i] = -1;
-
-    dist[src] = 0;
-
-    for (int count = 0; count < n - 1; count++) {
-        int u = minNode(dist, visited, n);
-        visited[u] = true;
-
-        for (int v = 0; v < n; v++) {
-            if (!visited[v] && g[u][v] != INF &&
-                dist[u] + g[u][v] < dist[v]) {
-
-                dist[v] = dist[u] + g[u][v];
-                parent[v] = u;
-            }
-        }
-    }
-
-    printf("\nShortest path from %c to %c:\n", src + 'a', dest + 'a');
-    printf("%c", src + 'a');
-    printPath(parent, dest);
-    printf("\nCost = %d\n", dist[dest]);
-}
-
-int main() {
-    int n, g[MAX][MAX];
-    char s, d;
-
-    printf("Number of nodes: ");
-    scanf("%d", &n);
-
-    printf("Adjacency matrix:\n");
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &g[i][j]);
-            if (g[i][j] == -1) g[i][j] = INF;
-        }
-
-    printf("Source (a,b,c...): ");
-    scanf(" %c", &s);
-    printf("Destination: ");
-    scanf(" %c", &d);
-
-    dijkstraLSR(g, n, s - 'a', d - 'a');
-    return 0;
+int main()
+{
+ int n,i,j,r;
+ int cost[MAX][MAX];
+ int distance[MAX];
+ int parent[MAX];
+ int visited[MAX];
+ 
+ printf("enter no of routers: ");
+ scanf("%d",&n);
+ printf("enter cost(-1 for no direct link): ");
+ for(i=0;i<n;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   printf("%d->%d:",i,j);
+   scanf("%d",&cost[i][j]);
+   if(cost[i][j]<0) cost[i][j]=INF;
+  }
+ }
+ int src;
+ printf("enter source router: ");
+ scanf("%d",&src);
+ for(i=0;i<n;i++)
+ {
+  distance[i]=cost[src][i];
+  parent[i]=src;
+  visited[i]=0;
+ }
+ visited[src]=1;
+ for(int step=0;step<n-1;step++)
+ {
+  int mindist=INF,nextrouter=-1;
+  for(r=0;r<n;r++)
+  {
+   if(!visited[r]&&distance[r]<mindist)
+   {
+    mindist=distance[r];
+    nextrouter=r;
+   }
+  }
+  if(nextrouter==-1) break;
+  visited[nextrouter]=1;
+  for(int r=0;r<n;r++)
+  {
+   if(!visited[r]&&
+      cost[nextrouter][r]!=INF&&
+      distance[nextrouter]+cost[nextrouter][r]<distance[r])
+   {
+    distance[r]=distance[nextrouter]+cost[nextrouter][r];
+    parent[r]=nextrouter;
+   }
+  }
+ }
+ printf("shortest path from route %d: ",src);
+ for(int dest=0;dest<n;dest++)
+ {
+  if(dest==src) continue;
+  printf("%d==>%d: Path: %d",src,dest,dest);
+  int p=dest;
+  while(p!=src)
+  {
+   printf("<--%d",parent[p]);
+   p=parent[p];
+  }
+  printf("\nCost: %d\n",distance[dest]);
+ }
+ return 0;
 }
